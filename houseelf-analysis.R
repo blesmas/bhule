a54753105e8f2c4898536e7bffc592a9d69c1e78
Blessing Masasi <blessing.masasi@okstate.edu>
2017-10-24 22:36:19 -0500

    Function to calculate GC_Content

--- houseelf-analysis.R
+++ houseelf-analysis.R
@@ -3,3 +3,19 @@
 #analysis to understand why house-elves possess powerful magic using DNA samples and ear measurements
 
 #3 Importing Data
+main_data <- read.csv("./data/houseelf_earlength_dna_data.csv", header = TRUE, sep = "," )
+
+#4 Commit Multiple Files
+main_data <- read.csv("./data/houseelf_earlength_dna_data_1.csv", header = TRUE, sep = "," )
+
+#5 Adding a Remote
+
+#6 Pushing changes
+#6.1 GC Content
+library(stringr)
+GC_CONTENT <- function(sequence){
+  Gs <- str_count(str_to_upper(sequence), "G")
+  Cs <- str_count(str_to_upper(sequence), "C")
+  gc_content <- (Gs + Cs) / str_length(sequence) * 100
+  return(gc_content)
+}
